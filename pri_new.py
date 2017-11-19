35adf72b18d49aa21ab4a4169b8a393cdeb0d59d
gt80 <geo.tse@hotmail.com>
2017-11-19 10:47:12 +0100

    PythonFIle
    test of print

---
+++ pri_new.py
@@ -0,0 +1,18 @@
+print("fy", end=" f ")
+print("fsdfy")
+xxx="potato inside"
+print (xxx.upper())
+print(""" fdfdf
+dfddfdfd
+dfdfffdf""")
+
+print(" aaafdfdf\
+fdfsgbvbbcvbc\
+tytryrt")
+
+
+games = int(input("Computer Games: "))
+
+total =  games
+
+print("\nGrand Total:", total)
